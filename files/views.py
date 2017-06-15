f7a0f568fef1860effbf448051705b1a0eebf429
Black <razorshift@yahoo.com>
2017-06-15 21:17:38 +0200

    Require authentication and ownership to view files

--- files/views.py
+++ files/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 from .models import File
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
+from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
 
 # Create your views here.
 
 def access_file(request, file_id):
-    file = File.objects.get(id=file_id)
-    return HttpResponse(file.get_str())
+    if request.user.is_authenticated:
+        file = File.objects.get(id=file_id)
+        if file.directory.project.user == request.user.id:
+            return HttpResponse(file.get_str())
+        else:
+           raise PermissionDenied
+    else:
+        raise PermissionDenied
 
 def index(request):
     template = loader.get_template('index.html')
