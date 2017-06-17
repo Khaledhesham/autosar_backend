60527274e7661e341dc4fc3fa9533af1c0fa4b08
Khaledhesham <khaledhesham22@gmail.com>
2017-06-17 09:16:53 +0200

    change serializer json format

--- files/serializers.py
+++ files/serializers.py
@@ -13,13 +13,22 @@ class FileSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('name', 'id', 'file_type')
 
 class DirectorySerializer(serializers.HyperlinkedModelSerializer):
-    directory_set = RecursiveField(many=True,required=False)
+    children = serializers.SerializerMethodField()
 
     class Meta:
         model = Directory
-        fields = ('name', 'id', 'file_set' , 'directory_set')
+        # fields = ('name', 'id', 'file_set' , 'directory_set')
+        fields = ('name', 'id', 'children')
 
-    file_set = FileSerializer(many=True, read_only=True)
+    def get_children(self, obj):
+        items = []
+        for item_file in obj.file_set.all():
+            item_file.type = 'file'
+            items.append(FileSerializer(instance=item_file).data)
+        for item_dir in obj.directory_set.all():
+            item_dir.type = 'folder'
+            items.append(DirectorySerializer(instance=item_dir).data)
+        return items
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
--- registration_system/views.py
+++ registration_system/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
-# Create your views here.
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import unicodedata
+
+class ExampleView(APIView):
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
+        content = {
+            'user': unicodedata(request.user),  # `django.contrib.auth.User` instance.
+            'auth': unicodedata(request.auth),  # None
+        }
+        return Response(content)
 
 def index(request):
     template = loader.get_template('registration/index.html')
