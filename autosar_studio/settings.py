90f1285b1324318fe1715b589a910ea5b7020fbf
Khaled Sakr <khaledosamasakr@gmail.com>
2017-09-22 01:45:41 +0200

    Revert "Temp revert"
    This reverts commit 842f93fa0a058298db1f9806f0714aaacf5e6c39.

--- autosar_studio/settings.py
+++ autosar_studio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from sys import platform
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -86,15 +87,28 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'OPTIONS': {
-            'timeout': 50,
+if platform == "linux" or platform == "linux2":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'autosarstudio',
+            'USER': 'autosar_backend',
+            'PASSWORD': '22598100',
+            'HOST': 'localhost',
+            'PORT': '',
         }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'OPTIONS': {
+                'timeout': 50,
+            }
+        }
+    }
+
 
 
 # Password validation
